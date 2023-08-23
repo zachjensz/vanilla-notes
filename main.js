51b13f871234dbf8871c75ca9a15a5c258bc4948
Zach Jensz <zach@zachjensz.com>
2023-08-23 18:33:15 +1000

    add todos, improved layout, extracted logic

--- main.js
+++ main.js
@@ -1,23 +1,23 @@
-const todos = [
-  {
-    title: 'Complete Vanilla Todos project',
-    description: 'Finish the odin project todo app project with vanilla JS',
-    dueDate: undefined,
-    priority: 1,
-  },
-];
+import { todos } from './logic.js';
 
 const el_todoList = document.querySelector('.todo-list');
+const el_createTodo = document.querySelector('.create-todo');
 const tp_todo = document.querySelector('#template-todo');
 
 todos.forEach((todo) => renderTodo(todo));
+el_createTodo.addEventListener('click', createTodo);
 
+function createTodo() {
+  todos.push({});
+  renderTodo({});
+}
 function renderTodo(todo) {
   const el_todo = tp_todo.content.cloneNode(true);
-  el_todo.querySelector('.title').innerText = todo.title;
-  el_todo.querySelector('.description').innerText = todo.description;
-  el_todo.querySelector('.dueDate').innerText = todo.dueDate;
-  el_todo.querySelector('.priority').innerText = todo.priority;
+  el_todo.querySelector('.title').innerText = todo.title ?? 'title';
+  el_todo.querySelector('.description').innerText =
+    todo.description ?? 'description';
+  el_todo.querySelector('.dueDate').innerText = todo.dueDate ?? 'no due date';
+  el_todo.querySelector('.priority').innerText = todo.priority ?? 'no priority';
   el_todoList.appendChild(el_todo);
 }
 /*
