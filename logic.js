2ed0a32d7f3e374603465c35b0d4e92af5f7de12
Zach Jensz <zach@zachjensz.com>
2023-08-24 18:05:44 +1000

    added extracted logic file

---
+++ logic.js
@@ -0,0 +1,8 @@
+export const todos = [
+  {
+    title: 'Complete Vanilla Todos project',
+    description: 'Finish the odin project todo app project with vanilla JS',
+    dueDate: undefined,
+    priority: 1,
+  },
+];
